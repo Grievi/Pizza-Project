c744543f6aa0d42608543ab1f2cb7f1f8efc5db1
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-01 22:13:58 +0300

    modify script.js, index.html,style.css and landing.html

--- js/script.js
+++ js/script.js
@@ -1,64 +1,130 @@
-let total = 0
-function Order( size, topping, crust){
-    
-    this.size = size;
-    this.topping = topping;
-    this.crust = crust;
-    this.cost = 0
-}
-Order.prototype.totalAmount = function(){
+const addButton = document.getElementById('add-btn');
+const list = document.getElementById('list');
+const order = document.getElementById('order');
+const priceInput = document.getElementById('price');
+const quantityInput = document.getElementById('quantity');
+const totalPrice = document.getElementById('total-price');
+const cart = document.getElementById('cart');
+const totalDiv = document.getElementById('total-div');
 
-    switch (this.pizza){
-        case 'large':
-            this.cost = 800;
-            break;
-        case 'medium':
-            this.cost = 600;
-            break;
-        case 'small':
-            this.cost = 400;
-            break;
-        default:
-            break;
-    }
-
-    switch(this.topping){
-        case 'pepparoni':
-            this.cost += 150;
-            break;
-        case 'mushroom':
-            this.cost += 150;
-            break;
-        case 'cheese':
-            this.cost += 150;
-            break;
-        default:
-            break;
-
-    }
-    
-}
-
-let pizzaOrder = [];
+const cartList = [];
 let total = 0;
 
-$('btnSubmit').click( function(event){
-    event.preventDefault();
-    let topping = $("#topping").children("option:selected").val();
-    let size = $("#size").children("option:selected").val();   let piza = $("#selectPizza").children("option:selected").val(); 
+const state = {};
+
+addButton.addEventListener('click', (e)=> {
+    e.preventDefault();
+    const meal = capitalize(order.value);
+    const mealPrice = priceInput.value;
+    const mealQuantity = quantityInput.value;
+    if(meal !== '' && !cartList.includes(meal)) {
+
+        state[`${meal}`] = { 
+            price: `${mealPrice}`,
+            quantity: `${mealQuantity}`
+        };
+
+        cartList.push(meal);
+        
+        renderList();
+
+        total += parseFloat(mealPrice) * parseInt(mealQuantity);
+        order.value = '';
+        quantityInput.value = '1';
+        priceInput.value = '';
+        renderTotal();
+    }
+});
+
+list.addEventListener('click', (e)=>{
+    const element = e.target;
+    if(element.classList[0] === 'button') {
+        const elementNode = element.parentElement;
+        const elementDataName = element.dataset.name;
+        let elementDataPrice = parseFloat(element.dataset.price);
+        let elementDataQuantity = parseInt(element.dataset.quantity);
+        total = total - (parseFloat(elementDataPrice) * parseInt(elementDataQuantity));
+        elementNode.remove();
+        if(cartList.includes(elementDataName))
+        {
+            const index = cartList.indexOf(elementDataName);
+            cartList.splice(index,1);
+            delete state[`${elementDataName}`];
+        }
+
+        renderList();
+        renderTotal();
+    }
+});
+
+totalPrice.addEventListener('click', () => {
+
+    totalDiv.classList.toggle('hidden');
+    renderTotal();
+    
 })
 
-var pizza = newPizza(size,topping, crust);
-pizza.totalAmount();
+// addButton.addEventListener('click', crustType);
 
-total += pizza.price;
+cart.addEventListener('click', () => {
+    list.classList.toggle('hidden');
+})
+
+const renderList = () => {
+    list.innerHTML = '';
+    
+    //console.log(quantityInput.value);
+    // Setting the items in alphabetical order
+    cartList.sort();
+    cartList.forEach((orderedMeal) => {        
+
+        itemHtml = `
+            <div class="mt-sm-3 bg-light alert flexview">
+                <p class="large">${orderedMeal}</p>
+                <button type="button" class="btn btn-danger col-sm-2 remove-btn" data-name="${orderedMeal}" data-price="${state[`${orderedMeal}`].price}" data-quantity="${state[`${orderedMeal}`].quantity}">Remove</button>
+            </div>`;    
+        list.insertAdjacentHTML('beforeend', itemHtml);
+    })
+}
 
 
-var order = `<div class="alert alert-secondary" role="alert">
-${pizza.size} ${pizza.topping} with ${pizza.crust} crust
-<span class="float-right font-weight-bold">${pizza.price}/=</span>
-</div>`;
+const crustOptions = document.querySelector('#crust-type');
+const Toppings = document.querySelector('#toppings');
 
+const renderTotal = () => {
+  
+    // CRUST TYPE
+
+    let crustCost;
+    let Toppings = 200;
+
+    if (crustOptions.value === 'crispy')
+    {
+        crustCost = 200;
+        alert('Your order will be delivered after processing...');
+    } else if (crustOptions.value === 'stuffed')
+    {
+        crustCost = 270;
+        alert('Your order will be delivered after processing...');
+    } else if (crustOptions.value === 'gluten-free')
+    {
+        crustCost = 300;
+        alert('Your order will be delivered after processing...');
+    }
+
+  
+
+    // TOTAL
+
+    totalDiv.innerHTML = '';
+    //const html = `Total : <span>${total}</span>`;
+    const html = `<p class="display-4">Your total is: <span>KES${total + crustCost + Toppings}</span></p>`;
+    totalDiv.insertAdjacentHTML('afterbegin', html);
+}
+
+const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 
 
