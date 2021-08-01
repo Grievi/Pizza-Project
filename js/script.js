ed181b496ca9e90085b17035a802103a45f0b8d2
Grievin <grievin.okumu@student.moringaschool.com>
2021-08-01 17:10:33 +0300

    add javascript fconstructors and object pizza prototype

--- js/script.js
+++ js/script.js
@@ -1,44 +1,60 @@
-$(document).ready(function(){
-    // alert("ready")
-
-})
-
-// let parallax = document.getElementsById("bana")
-
-// window.addEvent.addEventListener("scroll", function(){
-//  let offset = window.pageYOffset ;
-//  parallax.style.backgroundPositionY = offset*0.7 + "px";
-// })
-
-
-
-
-
-
-
-
-
-let pizza = document.getElementByClassName("pizza");
-let number = document.getElementsById("numPizza");
-let sizePizza = document.getElementsByClassName("size")
-let crust = document.getElementsByClassName("crust")
-let topping = document.getElementsByClassName("topping")
-
-if (pizza = document.getElementByClassName("pizza").selected.value){
-    return true
-}
-
-
-if (number = document.getElementsById("numPizza").selected.value)
-
-
+let total = 0
 function Order(pizza, number, size, topping, crust){
     this.pizza = pizza;
     this.number = number;
     this.size = size;
     this.topping = topping;
     this.crust = crust;
+    this.cost = 0
 }
+Order.prototype.total = function(){
+
+    switch (this.pizza){
+        case 'large':
+            this.cost = 800;
+            break;
+        case 'medium':
+            this.cost = 600;
+            break;
+        case 'small':
+            this.cost = 400;
+            break;
+        default:
+            break;
+    }
+
+    switch(this.topping){
+        case 'pepparoni':
+            this.cost += 150;
+            break;
+        case 'mushroom':
+            this.cost += 150;
+            break;
+        case 'cheese':
+            this.cost += 150;
+            break;
+        default:
+            break;
+
+    }
+    
+}
+
+let pizzaOrder = [];
+let total = 0;
+
+$('btnSubmit').click( function(event){
+    event.preventDefault();
+    
+})
+
+
+
+
+
+
+
+
 
 
 
